67df4e7b86160db71c3853ff9ca96f3033bd1335
Wagner Leonardi <leonardiwagner@gmail.com>
2019-12-19 14:05:23 -0300

    improve solution

--- src/crackin/greedy_difference.clj
+++ src/crackin/greedy_difference.clj
@@ -8,22 +8,14 @@
                    :else (+ (* -1 a) b))]
     (if (< distance 0) (* -1 distance) distance)))
     
-(defn min-number [arr number-index]
-  (reduce
-    (fn [minimum arr-index]
-      (if (= arr-index number-index)
-        minimum
-        (let [number (nth arr number-index)
-              current (nth arr arr-index)
-              distance (absolute-distance number current)]
-          (min distance minimum))))
-    1000000
-    (range 0 (count arr))))
-
 (defn execute [arr]
-  (reduce
-    (fn [minimum i]
-      (let [min-distance (min-number arr i)]
-        (min min-distance minimum)))
-    1000000
-    (range 0 (count arr))))
+  (let [list (sort arr)]
+    (reduce
+      (fn [minimum i]
+        (if (= i (dec (count list)))
+          (reduced minimum)
+          (let [me (nth list i)
+                next (nth list (inc i))]
+            (min minimum (absolute-distance me next)))))
+      1000000
+      (range (count list)))))
